13715260584d01ba22509f67ae489076df30ae57
Jeremiah Jones <jmjones@isolvedhcm.com>
2018-07-06 12:48:33 -0600

    rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Book.create!([
-  {title: "The Alphabet", pages: 26, author: "America, the Great", genre: "learning", user_id: 1},
-  {title: "Harry Potter Book 1", pages: 264, author: "J.K. Rolling", genre: "fiction", user_id: 1},
-  {title: "Harry Potter Book 2", pages: 264, author: "J.K. Rolling", genre: "fiction", user_id: 1},
-  {title: "Harry Potter Book 3", pages: 364, author: "J.K. Rolling", genre: "fiction", user_id: 1}
-])
+               { title: 'The Alphabet', pages: 26, author: 'America, the Great', genre: 'learning', user_id: 1 },
+               { title: 'Harry Potter Book 1', pages: 264, author: 'J.K. Rolling', genre: 'fiction', user_id: 1 },
+               { title: 'Harry Potter Book 2', pages: 264, author: 'J.K. Rolling', genre: 'fiction', user_id: 1 },
+               { title: 'Harry Potter Book 3', pages: 364, author: 'J.K. Rolling', genre: 'fiction', user_id: 1 }
+             ])
 User.create!([
-  {first_name: "Jimmy", last_name: "Johns", email: "jimmy-johns@gmail.com"},
-  {first_name: "Jane", last_name: "Johns", email: "jane-johns@gmail.com"},
-  {first_name: "Papa", last_name: "Johns", email: "papa-johns@gmail.com"},
-  {first_name: "apple", last_name: "bottom", email: "apple-bottom-jeans@hotmail.com"}
-])
+               { first_name: 'Jimmy', last_name: 'Johns', email: 'jimmy-johns@gmail.com' },
+               { first_name: 'Jane', last_name: 'Johns', email: 'jane-johns@gmail.com' },
+               { first_name: 'Papa', last_name: 'Johns', email: 'papa-johns@gmail.com' },
+               { first_name: 'apple', last_name: 'bottom', email: 'apple-bottom-jeans@hotmail.com' }
+             ])
